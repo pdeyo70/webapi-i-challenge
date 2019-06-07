aa8c68ff2590c11d2b415b85b6a10f22d76f89d2
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-06 20:22:38 -0500

    running into issue with POST

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require('express');
+
+// import express from 'express'
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => {
+    console.log('Working on port 5000')
+})
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+
+    if (userInfo.name == null || userInfo.bio == null) {
+        res.status(400).json({
+            success: false,
+            errorMessage: 'Please provide name and bio for the user.'
+        })
+    } else {
+        db.insert(userInfo)
+            .then(user => {
+                res.status(201).json({ success: true, user })
+            })
+            .catch(err => res.status(500).json({ success: false, error: 'There was an error while saving the user to the database.' }))
+    }
+
+
+
+})
+
