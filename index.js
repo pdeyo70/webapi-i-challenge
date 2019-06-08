07d3fb30a3246194de07fcb0418cf1acd426e515
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-08 11:45:50 -0500

    got POST working

--- index.js
+++ index.js
@@ -16,20 +16,17 @@ server.listen(5000, () => {
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
 
-    if (userInfo.name == null || userInfo.bio == null) {
-        res.status(400).json({
-            success: false,
-            errorMessage: 'Please provide name and bio for the user.'
-        })
-    } else {
+    if((userInfo.name == '' || userInfo.name == null) || (userInfo.bio == '' || userInfo.bio == null)){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    else {
         db.insert(userInfo)
             .then(user => {
-                res.status(201).json({ success: true, user })
+                res.status(201).json({ userInfo })
             })
-            .catch(err => res.status(500).json({ success: false, error: 'There was an error while saving the user to the database.' }))
+            .catch(err => {
+                res.status(500).json({ err })
+            })
     }
-
-
-
 })
 
