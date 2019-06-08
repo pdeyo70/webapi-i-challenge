16e46b3fbd5f0c045acab4c3bbd351ac8f58a70c
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-08 12:02:43 -0500

    got GET working on /users

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.listen(5000, () => {
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
 
-    if((userInfo.name == '' || userInfo.name == null) || (userInfo.bio == '' || userInfo.bio == null)){
+    if ((userInfo.name == '' || userInfo.name == null) || (userInfo.bio == '' || userInfo.bio == null)) {
         res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
     }
     else {
@@ -28,5 +28,15 @@ server.post('/api/users', (req, res) => {
                 res.status(500).json({ err })
             })
     }
+})
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        })
 })
 
