54923aa2497bbd2dfa9997d6a899fc95d9994a9f
Paul DeYonghe <p.deyonghe@gmail.com>
2019-06-08 13:16:13 -0500

    PUT request working, accomplishing MVP

--- index.js
+++ index.js
@@ -69,3 +69,26 @@ server.delete('/api/users/:id', (req, res) => {
             res.status(500).json({ error: "The user could not be removed" })
         })
 })
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db.update(id, changes)
+        .then(updated => {
+            if(updated) {
+                if ((changes.name == '' || changes.name == null) || (changes.bio == '' || changes.bio == null)) {
+                    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+                } else {
+                    res.status(200).json(changes)
+                }
+                
+            } else {
+                res.status(404).json({ error: "The user with the specified ID does not exist." })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be modified." })
+        })
+})
